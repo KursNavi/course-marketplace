a347c93ef5c373841dd84868acb11ff0d900f08e
KursNavi <BTRespondek@gmail.com>
2026-01-28 22:10:29 +0100

    Direktbuchungsfilter info fix

--- src/components/SearchPageView.jsx
+++ src/components/SearchPageView.jsx
@@ -353,10 +353,10 @@ const SearchPageView = ({
                                 <CreditCard className="w-3 h-3 text-green-500" />
                                 <Info className="w-3 h-3 text-gray-400 group-hover:text-green-500 transition-colors" />
                             </label>
-                            <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 px-3 py-2 bg-gray-900 text-white text-xs rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-50 shadow-lg">
+                            <div className="absolute top-full left-1/2 -translate-x-1/2 mt-2 px-3 py-2 bg-gray-900 text-white text-xs rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none w-72 text-center z-[100] shadow-lg">
+                                <div className="absolute bottom-full left-1/2 -translate-x-1/2 border-4 border-transparent border-b-gray-900"></div>
                                 <div className="font-semibold mb-1">{t.lbl_direct_booking_filter}</div>
                                 <div className="text-gray-300">{t.tooltip_direct_booking_long || t.tooltip_direct_booking}</div>
-                                <div className="absolute top-full left-1/2 -translate-x-1/2 border-4 border-transparent border-t-gray-900"></div>
                             </div>
                          </div>
                     </div>
