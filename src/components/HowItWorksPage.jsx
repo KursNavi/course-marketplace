5ff282bd09da1813500a01f92b525298b2ea1008
KursNavi <BTRespondek@gmail.com>
2026-03-09 22:22:24 +0100

    Remove FAQ entry about verified badge

--- src/components/HowItWorksPage.jsx
+++ src/components/HowItWorksPage.jsx
@@ -8,7 +8,7 @@ const HowItWorksPage = ({ t, setView }) => {
         setOpenFaq(openFaq === index ? null : index);
     };
 
-    const faqs = [1, 2, 3, 4, 5, 6, 11, 7, 8, 9, 10];
+    const faqs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     return (
         <div className="font-sans bg-beige min-h-screen">
